5b5a187f30bbb0d6146931c49c0e55a66559fcde
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-05-29 21:00:08 +0530

    Developed retrieving courses list on admin panel #1

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -2,19 +2,15 @@ import React, { useEffect, useState } from 'react'
 import AdminSideNavBar from '../components/adminpage_component/side_nav_bar_component/AdminSideNavBar'
 import AdminHoirzontalBar from '../components/adminpage_component/admin_horizontal_top_bar_component/AdminHoirzontalBar'
 import DashBoard from '../components/adminpage_component/dashboard_component/DashBoard'
+import AdminCourseList from '../components/adminpage_component/courses_component/AdminCourseList'
 
 export default function AdminPage() {
 
   const [isDahsboardClicked, setIsDashboardClicked] = useState(true)
 
+  const [isCoursesClicked, setIsCoursesClicked] = useState(false)
+
   const [courseID, setCourseID] = useState('')
-
-  const [isAdded, setIsAdded] = useState(false)
-  const [isUpdated, setIsUpdated] = useState(false)
-  const [isDeleted, setIsDeleted] = useState(false)
-
-  const [isDisplayAddForm, setIsDisplayAddForm] = useState(false)
-  const [isDisplayUpdateForm, setIsDisplayUpdateForm] = useState(false)
 
   useEffect(() => {
 
@@ -26,15 +22,28 @@ export default function AdminPage() {
       <div className='admin-page-side-nav-bar'>
 
         <AdminSideNavBar
+
           isDahsboardClicked={isDahsboardClicked}
           setIsDashboardClicked={setIsDashboardClicked}
+
+          isCoursesClicked={isCoursesClicked}
+          setIsCoursesClicked={setIsCoursesClicked}
         >
         </AdminSideNavBar>
       </div>
     
       <div className='admin-page-content-side'>
         <AdminHoirzontalBar></AdminHoirzontalBar>
-        <DashBoard isDahsboardClicked={isDahsboardClicked}></DashBoard>
+
+        <DashBoard 
+           isDahsboardClicked={isDahsboardClicked}
+
+        ></DashBoard>
+
+        <AdminCourseList
+          isCoursesClicked={isCoursesClicked}
+          setIsCoursesClicked={setIsCoursesClicked}
+        ></AdminCourseList>
       </div>
 
     </div>
